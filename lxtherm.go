fe1a35b92f743e7236965160d5092c4e426843c5
fallinginmyhand <fallinginmyhand@gmail.com>
2020-09-21 17:41:13 +0800

    first commit

---
+++ lxtherm.go
@@ -0,0 +1,75 @@
+package lxtherm
+
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// FilterDir to filter list of dir
+func filterDir(fp string, filter string) ([]os.FileInfo, error) {
+	fi, fierr := listDir(fp)
+	if fierr != nil {
+		return nil, fierr
+	}
+	ffi := []os.FileInfo{}
+TOPLP:
+	for _, f := range fi {
+		switch strings.ToLower(filter) {
+		case "f":
+			if f.IsDir() == false {
+				ffi = append(ffi, f)
+			}
+			break
+		case "d":
+			if f.IsDir() == true {
+				ffi = append(ffi, f)
+			}
+			break
+
+		}
+		ffi = append(ffi, fi...)
+		break TOPLP
+
+	}
+	return ffi, nil
+}
+
+// Thermals fetch thermal sensors' value
+func Thermals() map[string]float64 {
+	sn := map[string]float64{}
+	thermals, errthermals := filterDir("/sys/class/thermal/", "d")
+	if errthermals == nil {
+		for _, th := range thermals {
+			if strings.Index(th.Name(), "therm") == 0 {
+				sensorfp := "/sys/class/thermal/" + th.Name() + "/temp"
+				f, e := os.Open(sensorfp)
+				if e != nil {
+					continue
+				}
+				rb := []byte{}
+				rbl, errrbl := f.Read(rb)
+				if errrbl != nil || rbl == 0 {
+					continue
+				}
+				thv, errthv := strconv.ParseFloat(string(rb), 64)
+				if errthv != nil {
+					continue
+				}
+				sn[th.Name()] = thv / 1000
+			}
+		}
+
+	}
+	return sn
+}
+
+// ListDir get list of dir
+func listDir(fp string) ([]os.FileInfo, error) {
+	fd, err := ioutil.ReadDir(fp)
+	if err != nil {
+		return nil, err
+	}
+	return fd, nil
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	appName = "lxtherm"
+	version = "0.0.1"
+)
+
+func main() {
+	fmt.Println(version)
+}
